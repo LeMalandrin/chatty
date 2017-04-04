b83d2b5f8e1eddf90644a8ce62335ba34d49da72
LeMalandrin <coucke.antony@gmail.com>
2017-04-04 14:40:46 +0200

    Finalisation du composant login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
+	user:User = new User();
+	errors:string[] = [];
+	controls:string[] = [];
 
-  constructor() { }
+  	constructor(private userService:UserService, private router:Router) {
+  		this.controls['login'] = "form-group";
+  		this.controls['password'] = "form-group";
+  	}
 
-  ngOnInit() {
-  }
+  	ngOnInit() {
+  	}
+
+
+  	connect() {  		
+  		this.controls['login'] = "form-group";
+  		this.controls['password'] = "form-group";
+
+  		this.userService.setUser(this.user);
+  		let login = this.userService.login();
+  		if(login != null) {
+  			login.then((auth)=>{
+  				this.userService.setMe(auth.uid);
+	  			this.router.navigate(['']);
+  			}).catch((error)=>{  				
+	  			this.errors['password'] = (this.errors['password']!=undefined) ? this.errors['password'] : [];
+	  			this.errors['password'].push("Le mot de passe est invalide");
+	  			this.controls['password'] = "form-group has-error";
+  			})
+  		} else {
+  			this.errors['login'] = (this.errors['login']!=undefined) ? this.errors['login'] : [];
+  			this.errors['login'].push("L'adresse email ou le nom d'utilisateur n'existe pas");
+	  		this.controls['login'] = "form-group has-error";
+  		}
+  	}
 
 }
