f1e63b6618b93927c54e32dd4108feddc2a8e0a3
LeMalandrin <coucke.antony@gmail.com>
2017-04-04 13:56:06 +0200

    Ajout de la route /login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes = [
 	{
 		path: 'register',
 		component: RegisterComponent
-	}
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	}	
 ];
