bcd03658a0f4a5c6aa7e2f8cca894764c07008cf
LeMalandrin <coucke.antony@gmail.com>
2017-04-04 16:18:40 +0200

    Dernières modification du composant navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user/user.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [UserService]
 })
 export class NavbarComponent implements OnInit {
+	me:any;
+  	constructor(private userService:UserService) {
+  	}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  	ngOnInit() {
+  		this.me = this.userService.getMe(localStorage.getItem('me'));
+  	}
 
 }
