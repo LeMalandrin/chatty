bc27f3a8017f7f26f747e568f929528ed3d15327
LeMalandrin <coucke.antony@gmail.com>
2017-04-04 13:09:22 +0200

    Modification du model User

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,15 +1,21 @@
 export class User {
 	public_id:string;
+	private_id:string;
 	email:string;
 	username:string;
 	password:string;
 	password_confirm:string;
+	status:string;
+	role:string;
 
 	constructor() {
 		this.public_id = "";
+		this.private_id = "";
 		this.email = "";
 		this.username = "";
 		this.password = "";
 		this.password_confirm = "";
+		this.status = "idle";
+		this.role = "basic";
 	}
 }
