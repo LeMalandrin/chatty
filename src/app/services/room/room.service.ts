6b8e57def5f3a63d9a06b5d649477c1e7726d980
LeMalandrin <coucke.antony@gmail.com>
2017-04-21 16:17:45 +0200

    Service Roomlist : ajout de la methode getRooms()

--- src/app/services/room/room.service.ts
+++ src/app/services/room/room.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Room } from '../../models/room';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Injectable()
 export class RoomService {
+	room:Room;
+	rooms:any[];
+	repository:FirebaseListObservable<any>;
 
-  constructor() { }
+	constructor(private af: AngularFire) {
+		this.repository = af.database.list('/rooms');
+		af.database.list('/rooms').subscribe(rooms=>{
+			this.rooms = rooms;
+		})
+	}
+
+
+	getRooms() {	
+		return this.af.database.list('/rooms');
+	}
 
 }
