e9452e9885646030cc2b6a69dbd3c7c6c64df1ab
LeMalandrin <coucke.antony@gmail.com>
2017-04-21 13:39:09 +0200

    Insertion de l'image de chargement sur le template du composant Chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
+  providers: [UserService]
 })
 export class ChatComponent implements OnInit {
+	me:any;
+  	constructor(private userService:UserService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  	ngOnInit() {  	
+    	let my_id = localStorage.getItem('me');
+    	if(my_id != null) {
+  			this.me = this.userService.getMe(my_id).subscribe(me=>{
+	        	this.me = me;
+	      	});
+    	}
+  	}
 
 }
