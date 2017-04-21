c2d194bf83441116e39d740f2c96a3b5730a4c26
LeMalandrin <coucke.antony@gmail.com>
2017-04-21 16:18:37 +0200

    Composant Roomlist : appel de getRooms() dans le onInit

--- src/app/roomlist/roomlist.component.ts
+++ src/app/roomlist/roomlist.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Room } from '../models/room';
+import { RoomService } from '../services/room/room.service';
 
 @Component({
   selector: 'app-roomlist',
   templateUrl: './roomlist.component.html',
-  styleUrls: ['./roomlist.component.css']
+  styleUrls: ['./roomlist.component.css'],
+  providers: [RoomService]
 })
 export class RoomlistComponent implements OnInit {
 	@Input() me:any;
+	rooms:any[];
 
-  	constructor() { }
+  	constructor(private roomService:RoomService) { }
 
   	ngOnInit() {
+  		this.roomService.getRooms().subscribe(rooms=>{
+			this.rooms = rooms;
+		})
   	}
 
 }
