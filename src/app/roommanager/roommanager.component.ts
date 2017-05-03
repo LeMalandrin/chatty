97c6a9cd2b9ffcbb2e669e6ccb2d78551a30db99
LeMalandrin <coucke.antony@gmail.com>
2017-05-03 18:44:10 +0200

    Création du component RoommanagerComponent

---
+++ src/app/roommanager/roommanager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roommanager',
+  templateUrl: './roommanager.component.html',
+  styleUrls: ['./roommanager.component.css']
+})
+export class RoommanagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
