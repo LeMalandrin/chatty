b2c56fe528875da4922ed622eafa7b6d2336a2ca
LeMalandrin <coucke.antony@gmail.com>
2017-05-10 15:52:17 +0200

    Component Roommanager : ajout du module Ng2Bs3Modal

--- src/app/roommanager/roommanager.component.ts
+++ src/app/roommanager/roommanager.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { RoomService } from '../services/room/room.service';
+import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 @Component({
   selector: 'app-roommanager',
   templateUrl: './roommanager.component.html',
-  styleUrls: ['./roommanager.component.css']
+  styleUrls: ['./roommanager.component.css'],
+  providers: [RoomService]
 })
 export class RoommanagerComponent implements OnInit {
 	@Input() me:any;
-  	
-  	constructor() { }
+  	isListVisible:boolean = true;
+  	isAddModalVisible:boolean = false;
+	rooms:any[];
+
+    @ViewChild('modal')
+    modal: ModalComponent;
+
+
+  	constructor(private roomService:RoomService) { }
 
   	ngOnInit() {
+  		this.roomService.getRooms().subscribe(rooms=>{
+      		this.rooms = rooms;
+      	});
+  	}
+
+  	showAddModal() {
+  		this.isListVisible = false;
+  		this.isAddModalVisible = true;
   	}
 
 }
