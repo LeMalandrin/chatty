0b8de354e87bd96cffc6877d9ebc2b5b7ff191b8
LeMalandrin <coucke.antony@gmail.com>
2017-05-11 15:59:34 +0200

    Service Room : Ajout des fonction de validations, de création, d'édition et de suppression d'un salon

--- src/app/services/room/room.service.ts
+++ src/app/services/room/room.service.ts
@@ -15,14 +15,65 @@ export class RoomService {
 		})
 	}
 
+	getRoom(public_id) {      
+		for(var room of this.rooms) {
+			if(room.public_id === public_id) {
+				return room;
+			}
+		}
+		return false;
+	}
 
 	getOccupants(query) {
 		return this.af.database.list('/rooms_users', {
 	    	query
-	    })
+	    });
 	}
 	getRooms() {	
 		return this.af.database.list('/rooms');
 	}
+
+	create() {
+		if(this.isValid()) {
+			this.repository.push({
+				public_id: this.rand() + this.rand(),
+				label: this.room.label,
+				description: this.room.description
+			});
+			return true;
+		} else { return false; }
+	}
+
+	deleteRoom(public_id) {
+		let private_id = this.getRoom(public_id).$key;
+		this.af.database.object('/rooms/' + private_id).remove();
+	}
+
+
+	update() {
+		if(this.isValid()) {
+			let private_id = this.getRoom(this.room.public_id).$key;
+			this.af.database.object('/rooms/' + private_id).update({
+				label: this.room.label,
+				decription: this.room.description
+			});
+			return true;
+		} else { return false; }
+	}
+
+
+
+	isValid() {
+		let isValidLabel = (this.isValidLabel());
+		return isValidLabel;
+	}
+
+	isValidLabel() {
+		return this.room.label.match(/^[A-Za-z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ\_\-\s]{1,30}$/);
+	}
+
+	rand = function(){
+	    return Math.random().toString(36).substr(2); // remove `0.`
+	};
 
 }
