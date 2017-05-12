b671d08340b7051d48ba7d13f1a2fd7c7ba954f3
LeMalandrin <coucke.antony@gmail.com>
2017-05-12 11:11:27 +0200

    Ajout d'un bouclier au component Admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [UserService]
 })
 export class AdminComponent implements OnInit {
-	isRoomManagerVisible:boolean = true;
-	isUserManagerVisible:boolean = false;
-
-  	constructor() { }
+	  isRoomManagerVisible:boolean = true;
+	  isUserManagerVisible:boolean = false;
+    me:any;
+    logged:boolean;
+  
+  	constructor(private userService:UserService) {      
+      let my_id = localStorage.getItem('me');
+      if(my_id != null) {
+        this.logged=true;
+        this.me = this.userService.getMe(my_id).subscribe(me=>{
+          this.me = me;
+        });
+      } else {
+        this.logged=false;
+      }
+    }
 
 
   	ngOnInit() {
