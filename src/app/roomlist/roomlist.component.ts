89e6748da3331d23f998a7d3465726e9d22da862
LeMalandrin <coucke.antony@gmail.com>
2017-05-16 16:22:24 +0200

    Component Roomlist : ajout du component angular2-contextmenu

--- src/app/roomlist/roomlist.component.ts
+++ src/app/roomlist/roomlist.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Room } from '../models/room';
 import { RoomService } from '../services/room/room.service';
 import { UserService } from '../services/user/user.service';
+import { ContextMenuComponent } from 'angular2-contextmenu/angular2-contextmenu';
 
 @Component({
   selector: 'app-roomlist',
@@ -13,29 +14,36 @@ export class RoomlistComponent implements OnInit {
 	@Input() me:any;
 	rooms:any[];
 
-  	constructor(private roomService:RoomService, private userService:UserService) { }
-
-  	ngOnInit() {
-  		this.loadRoomlist();
-  	}
-
-    loadRoomlist() {
-      this.roomService.getRooms().subscribe(rooms=>{
-        this.rooms = rooms;
-        this.rooms.forEach(room=> {
-          var room_id = room.public_id;
-           room.occupants = [];
+  public items = [
+      { name: 'John', otherProperty: 'Foo' },
+      { name: 'Joe', otherProperty: 'Bar' }
+  ];
+  @ViewChild(ContextMenuComponent) public basicMenu: ContextMenuComponent;
 
 
-          this.roomService.getOccupantsByRoomId(room_id).subscribe(rooms_users=>{
-            rooms_users.forEach(room_user=>{
-              this.userService.getUser(room_user.user_id).subscribe(occupant=>{
-                room.occupants.push(occupant);
-              })              
-            })
-          });
-        })
-      });
-    }
+	constructor(private roomService:RoomService, private userService:UserService) { }
+
+	ngOnInit() {
+		this.loadRoomlist();
+	}
+
+  loadRoomlist() {
+    this.roomService.getRooms().subscribe(rooms=>{
+      this.rooms = rooms;
+      this.rooms.forEach(room=> {
+        var room_id = room.public_id;
+         room.occupants = [];
+
+
+        this.roomService.getOccupantsByRoomId(room_id).subscribe(rooms_users=>{
+          rooms_users.forEach(room_user=>{
+            this.userService.getUser(room_user.user_id).subscribe(occupant=>{
+              room.occupants.push(occupant);
+            })              
+          })
+        });
+      })
+    });
+  }
 
 }
