934a9cdfc52393fc00830177f1ea9daffe917f93
LeMalandrin <coucke.antony@gmail.com>
2017-05-16 14:14:08 +0200

    Component Logout : appel de la méthode deleteOccupantByuserId

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../services/user/user.service';
+import { RoomService } from '../services/room/room.service';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css'],
-  providers: [UserService]
+  providers: [UserService, RoomService]
 })
 export class LogoutComponent implements OnInit {
 
-  constructor(private userService:UserService, private router:Router) {
+  constructor(private userService:UserService, private roomService:RoomService, private router:Router) {
   	if(localStorage.getItem('me')) {     
         userService.logout();   
+        roomService.deleteOccupantByUserId(localStorage.getItem('me'));
         localStorage.removeItem('me');
     }
 
