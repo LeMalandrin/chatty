510106068e8e774d1a2a9a8fda7ab99f490f5088
LeMalandrin <coucke.antony@gmail.com>
2017-05-18 11:44:08 +0200

    Modification du component Roomlist : liaison avec le component userlist qui prend en paramètre le salon afin de récupèrer ses occupants

--- src/app/roomlist/roomlist.component.ts
+++ src/app/roomlist/roomlist.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Room } from '../models/room';
 import { RoomService } from '../services/room/room.service';
 import { UserService } from '../services/user/user.service';
-import { ContextMenuComponent } from 'angular2-contextmenu/angular2-contextmenu';
+import { ContextMenuComponent } from 'ngx-contextmenu/';
 
 @Component({
   selector: 'app-roomlist',
@@ -30,19 +30,6 @@ export class RoomlistComponent implements OnInit {
   loadRoomlist() {
     this.roomService.getRooms().subscribe(rooms=>{
       this.rooms = rooms;
-      this.rooms.forEach(room=> {
-        var room_id = room.public_id;
-         room.occupants = [];
-
-
-        this.roomService.getOccupantsByRoomId(room_id).subscribe(rooms_users=>{
-          rooms_users.forEach(room_user=>{
-            this.userService.getUser(room_user.user_id).subscribe(occupant=>{
-              room.occupants.push(occupant);
-            })              
-          })
-        });
-      })
     });
   }
 
