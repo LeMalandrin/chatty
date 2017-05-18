eb0be1d6511f599a7d89c5090179cfa5332e706c
LeMalandrin <coucke.antony@gmail.com>
2017-05-18 15:59:40 +0200

    Service Message : création des méthodes setMessage et create

--- src/app/services/message/message.service.ts
+++ src/app/services/message/message.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Message } from '../../models/message';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Injectable()
 export class MessageService {
+	message:any;
+	messages:any[];
+	repository:FirebaseListObservable<any>;
 
-  constructor() { }
+  	constructor(private af: AngularFire) {  		
+		this.repository = af.database.list('/messages');
+  	}
 
+  	create() {
+  		this.repository.push({
+  			public_id: this.rand() + this.rand(),
+  			room_id: this.message.room_id,
+  			user_id: this.message.user_id,
+  			content: this.message.content
+  		});
+  	}
+
+  	setMessage(message) {
+  		this.message = message;
+  	}
+
+	rand = function(){
+	    return Math.random().toString(36).substr(2); // remove `0.`
+	};
 }
