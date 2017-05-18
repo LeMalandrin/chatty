b4b4b4769735386d9eb9c633edfdab7e570efee8
LeMalandrin <coucke.antony@gmail.com>
2017-05-18 16:00:55 +0200

    Component Chatform : ajout de la méthode sendMessage

--- src/app/chatform/chatform.component.ts
+++ src/app/chatform/chatform.component.ts
@@ -1,23 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Message } from '../models/message';
+import { MessageService } from '../services/message/message.service';
 
 @Component({
   selector: 'app-chatform',
   templateUrl: './chatform.component.html',
-  styleUrls: ['./chatform.component.css']
+  styleUrls: ['./chatform.component.css'],
+  providers: [MessageService]
 })
 export class ChatformComponent implements OnInit {
-	message:Message;
+	message:Message = new Message();
 	@Input() me:any;
-  	constructor() {
+  	constructor(private messageService:MessageService) {
+  		
   	}
 
  	ngOnInit() {
  	}
 
  	sendMessage() {
+ 		this.message.user_id = this.me.public_id;
+ 		this.message.room_id = this.me.room_id;
 
-  		console.log(this.me);
+  		if(this.message.content != "") {
+ 			this.messageService.setMessage(this.message);
+ 			this.messageService.create();
+  		}
  	}
 
 }
