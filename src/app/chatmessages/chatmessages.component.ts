83d41bef6623adc3f521dc1d774c92b717eb9a37
LeMalandrin <coucke.antony@gmail.com>
2017-05-18 16:15:11 +0200

    Component Chatmessages : créationd e la variable input me

--- src/app/chatmessages/chatmessages.component.ts
+++ src/app/chatmessages/chatmessages.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '../models/message';
+import { MessageService } from '../services/message/message.service';
 
 @Component({
   selector: 'app-chatmessages',
   templateUrl: './chatmessages.component.html',
-  styleUrls: ['./chatmessages.component.css']
+  styleUrls: ['./chatmessages.component.css'],
+  providers: [MessageService]
 })
 export class ChatmessagesComponent implements OnInit {
+	@Input() me:any;
+  	constructor(private messageService:MessageService) { 
+  	}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  	ngOnInit() {  
+  		this.messageService.getMessages(this.me.room_id);	
+  	}
 
 }
