f25b43f72ce7eb4beb436362d2ee9d010a9096af
LeMalandrin <coucke.antony@gmail.com>
2017-05-19 16:08:17 +0200

    Component Chatmessages : récupération des messages et de l'utilisateur associé, ainsi que la date de création"

--- src/app/chatmessages/chatmessages.component.ts
+++ src/app/chatmessages/chatmessages.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Message } from '../models/message';
 import { MessageService } from '../services/message/message.service';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-chatmessages',
   templateUrl: './chatmessages.component.html',
   styleUrls: ['./chatmessages.component.css'],
-  providers: [MessageService]
+  providers: [MessageService, UserService]
 })
 export class ChatmessagesComponent implements OnInit {
 	@Input() me:any;
-  	constructor(private messageService:MessageService) { 
+	messages:any[] = [];
+
+  	constructor(private messageService:MessageService, private userService:UserService) { 
   	}
 
   	ngOnInit() {  
-  		this.messageService.getMessages(this.me.room_id);	
+  		this.messageService.getMessages(this.me.room_id).subscribe(messages=>{
+  			this.messages = [];
+  			messages.forEach(message=> {
+  				this.userService.getuserByPublicId(message.public_id).subscribe(user=> {
+  					message.user = user[0];
+  					this.messages.push(message);
+  					//console.log(this.messages);
+  				});
+  			})
+  		});	
   	}
 
 }
